04ab4409e3a166e163a6ae04b045e9121a34d91a
Karsten <admin@TA-8211-05>
2018-01-23 13:34:34 +0100

    metoder

---
+++ Loops/Metoder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Metoder
+    {
+        static void Main(string[] args)
+        {
+
+            // 10-tabel
+            for (int x = 1; x < 11; x++)
+            {
+                for (int y = 1; y < 11; y++)
+                {
+                    string number = (x * y).ToString();
+
+                    if ((x*y) > 50)
+                    
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        
+                    
+                    else
+                    
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(number.PadLeft(5));
+                
+
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
--- String2/String.cs
+++ String2/String.cs
@@ -14,10 +14,21 @@ namespace String2
             string fornavn = "Mikkel";
             string efternavn = "Cronberg";
 
-            StringBuilder FullName = new StringBuilder( fornavn + efternavn);
+            StringBuilder FullName = new StringBuilder( fornavn + " " + efternavn);
             Console.WriteLine(FullName);
 
+            string navnStort =  FullName.ToString().ToUpper();
+            Console.WriteLine(navnStort);
 
+            string navnLille = FullName.ToString().ToLower();
+            Console.WriteLine(navnLille);
+
+            string del = FullName.ToString().Substring(7, 4);
+            Console.WriteLine(del);
+
+            
+            
+           
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
