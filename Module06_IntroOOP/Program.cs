9d26a1e28a5354193bcf7e2e78fb74272a740d4b
Modul 6 <admin@TA-829-04>
2018-02-05 11:34:07 +0100

    module 6

--- Module06_IntroOOP/Program.cs
+++ Module06_IntroOOP/Program.cs
@@ -11,6 +11,18 @@ namespace Module06_IntroOOP
         static void Main(string[] args)
         {
 
+            //Lav en ny person og beregn alder
+
+            Person p1 = new Person();
+            p1.Efternavn = "Hansen"; //Console.ReadLine();
+            p1.Fornavn = "Karsten"; //Console.ReadLine();
+            p1.Fødselsår = 1983;
+            Console.WriteLine(p1.FuldtNavn());
+            Console.WriteLine(p1.BeregnetAlder());
+
+
+            
+
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 System.Console.Write("Press any key to continue . . . ");
@@ -18,4 +30,53 @@ namespace Module06_IntroOOP
             }
         }
     }
+
+    class Person
+        {
+
+        public string Fornavn;
+        public string Efternavn;
+        public int Fødselsår;
+        
+
+            public string FuldtNavn()
+            {
+
+            return $"{Fornavn} {Efternavn}";
+
+        
+            }
+
+            public int BeregnetAlder()
+               {
+
+                 int år = DateTime.Now.Year;
+                return år - this.Fødselsår;
+
+                 }
+     
+            //default constructor
+
+            public Person()
+            {
+                this.Fornavn = "";
+
+                this.Efternavn = "";
+
+            }
+
+        //Custom constructor
+
+        public Person(String efternavn, string fornavn, int fødselsår)
+        {
+            this.Fødselsår = fødselsår;
+            this.Fornavn = fornavn.ToUpper();
+            this.Efternavn = efternavn.ToUpper();
+        }
+
+     }
+
 }
+
+
+
