39f72e7007bd3436b613d74c8aa08b7232d80248
Modul 6 <admin@TA-829-04>
2018-02-06 10:48:21 +0100

    modul7

---
+++ Meyer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            terninger t = new terninger();
+            t.Terning1 = 3;
+            t.Terning2 = 2;
+
+
+            var slag = "Du slog: ["+t.Terning1+"]" + " og " + "["+t.Terning2+"]";
+
+            Console.WriteLine(slag);
+            Console.Read();
+        }
+    }
+    class terninger
+    {
+        private int terning1;
+
+        public int Terning1
+        {
+            get { return terning1; }
+            set {
+                if (terning1 >0 || terning1 >7)
+                {
+                    terning1 = 1;
+                }
+
+                terning1 = value; }
+        }
+
+        private int terning2;
+
+        public int Terning2
+        {
+            get { return terning2; }
+            set { terning2 = value; }
+        }
+
+
+    }
+}
---
+++ Vare/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vare v = new Vare();
+            v.Navn = "Vare #1";
+            v.Pris = 100;
+            Console.WriteLine("    " +v.Navn);
+            Console.WriteLine("    " + v.Pris + ".- uden moms");
+            Console.WriteLine("    " + v.PrisMedMoms() + ".- med moms ");
+
+            Vare v2 = new Vare("vare #2", 200);
+            Console.WriteLine("    " + v2.Navn);
+            Console.WriteLine("    " + v2.Pris + ".- uden moms");
+            Console.WriteLine("    " + v2.PrisMedMoms() + ".- med moms ");
+
+            Console.Read();
+        }
+    }
+
+   
+    class Vare
+
+    {
+        //Custom constructor
+        public Vare(string navn, double pris)
+        {
+            this.Pris = pris;
+            this.Navn = navn;
+        }
+
+        //Åben constructor (ctor to gange tab)
+        public Vare()
+        {
+        }
+
+
+
+        private string navn;
+
+        public string Navn
+        {
+            get {
+                Console.WriteLine("Nu aflæses der navn:");
+                return navn; }
+            set {
+                Console.WriteLine("Nu tildeles der et navn:");
+                navn = value; }
+        }
+
+        private double pris;
+
+        public double Pris
+        {
+            get {
+                Console.WriteLine("Nu aflæses der pris:");
+                return pris; }
+            set {
+                Console.WriteLine("Nu tildeles der en pris:");
+                pris = value; }
+        }
+
+       public double PrisMedMoms()
+        {
+            Console.WriteLine("Nu lægges moms til prisen");
+            return this.pris * 1.25;
+        }
+
+        
+    }
+}
